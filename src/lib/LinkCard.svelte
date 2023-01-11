68c79ed6c1f66895a1da89106a551a399cc20209
João Miguel Marinho da Cunha <joaocunha@Joaos-MacBook-Pro.local>
2023-01-11 11:51:28 +0000

    Add links data and icons rendering

---
+++ src/lib/LinkCard.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import type { ComponentType, SvelteComponentTyped } from 'svelte/internal';
+
+	export let href: string;
+	export let title: string;
+	export let icon: ComponentType<SvelteComponentTyped<any>> | undefined = undefined;
+</script>
+
+<a
+	{href}
+	target="_blank"
+	rel="noopener noreferrer"
+	class="w-full flex items-center p-1 rounded-md hover:scale-105 transition-all bg-zinc-100 mb-3 max-w-3xl"
+>
+	<div class="w-full flex text-center">
+		{#if icon}
+			<div class="w-10 h-10">
+				<svelte:component this={icon} class="rounded-sm w-10 h-10" />
+			</div>
+		{/if}
+
+		<h2 class="w-full flex justify-center items-center font-semibold text-zinc-700 -ml-10">
+			{title}
+		</h2>
+	</div>
+</a>
---
+++ src/routes/+page.server.ts
@@ -0,0 +1,51 @@
+import type { PageServerLoad } from './$types';
+
+type PossibleIcon = 'twitch' | 'youtube' | 'twitter' | 'instagram' | 'tiktok';
+
+interface Link {
+	href: string;
+	title: string;
+	icon: PossibleIcon;
+}
+
+interface Data {
+	links: Link[];
+}
+
+export const load = (async ({ setHeaders }) => {
+	// cache the page for the same length of time
+	// as the underlying data
+	setHeaders({
+		'cache-control': `s-maxage=${1000 * 60 * 60 * 24 * 5}`
+	});
+
+	return {
+		links: [
+			{
+				title: 'Twitch',
+				href: 'https://twitch.tv/lokuzt_sw',
+				icon: 'twitch'
+			},
+			{
+				title: 'Youtube',
+				href: 'https://www.youtube.com/c/LokuztSW',
+				icon: 'youtube'
+			},
+			{
+				title: 'Twitter',
+				href: 'https://twitter.com/joao_cnh',
+				icon: 'twitter'
+			},
+			{
+				title: 'Instagram',
+				href: 'https://www.instagram.com/lokuzt_sw/',
+				icon: 'instagram'
+			},
+			{
+				title: 'TikTok',
+				href: 'https://www.tiktok.com/@lokuzt_sw',
+				icon: 'tiktok'
+			}
+		]
+	};
+}) satisfies PageServerLoad<Data>;
