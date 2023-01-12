0fc072e0bbbfb3de92539dcbc5b5bd96a06def5d
João Miguel Marinho da Cunha <joaocunha@Joaos-MacBook-Pro.local>
2023-01-12 11:52:06 +0000

    Add twitter metatags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,20 @@
 <script>
 	import '../app.css';
+
+	import logo from '$lib/assets/logo.jpeg';
+
+	export const title = 'Lokuzt link-tree';
+	export const description = "Lokuzt's Social Links & Contacts";
 </script>
 
 <svelte:head>
-	<title>Lokuzt Linktree</title>
-	<meta name="description" content="Lokuzt's Social Links & Contacts" />
+	<title>{title}</title>
+	<meta name="description" content={description} />
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content="https://lokuzt-tree.vercel.app/" />
+	<meta property="twitter:title" content={title} />
+	<meta property="twitter:description" content={description} />
+	<meta property="twitter:image" content={logo} />
 </svelte:head>
 
 <slot />
