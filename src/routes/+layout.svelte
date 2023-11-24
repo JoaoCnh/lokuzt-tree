f2f79572a556fb8103b3d9ad760f4fff5bafabad
JoaoCnh <joaomarinho.cunha@gmail.com>
2023-11-24 14:23:34 +0000

    Add missing metatags and prepare html

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,9 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<title>Lokuzt Linktree</title>
+	<meta name="description" content="Lokuzt's Social Links & Contacts" />
+</svelte:head>
+
 <slot />
